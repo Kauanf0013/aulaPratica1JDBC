b1aa9cf12ea9931410611ef728e0b21f5d05cbaa
Kauanf0013 <kauanfelipe200814@gmail.com>
2026-04-29 09:57:03 -0300

    O negocio ta ficando brabo

--- src/main/java/org/example/db/DB.java
+++ src/main/java/org/example/db/DB.java
@@ -2,18 +2,38 @@ package org.example.db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
+   public static Connection conn = null;// conectar com o banco de daos
+
+    public static Connection getConnection() {// de fato vai fazer a conexão
+        if(conn== null){//
+            try{
+                Properties props = loadPropetiers();
+                String  url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props );
+
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+
+        }
+        return conn;
+    }
 
     public static Properties loadPropetiers() {
-        try (FileInputStream fs = new FileInputStream("db.properties")){
-        Properties props = new Properties();
-        props.load(fs);
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
 
-        return props;
-    } catch (IOException e){
-        throw new DbException(e.getMessage());
-}
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
 
-}
+    }}
